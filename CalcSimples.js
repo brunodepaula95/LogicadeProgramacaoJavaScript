4bf67dca22322e8412932ea11befac6de723eee8
Bruno de Paula <brunohpaula1995@gmail.com>
2024-08-06 15:55:07 -0300

    Iniciando os estudos

---
+++ CalcSimples.js
@@ -0,0 +1,5 @@
+var primeiro = parseInt(prompt("Digite o primeiro número"));
+var segundo = parseInt(prompt("Digite o segundo número"));
+var terceiro = parseInt(prompt("Digite o terceiro número"));
+var resultado = primeiro * segundo + terceiro;
+console.log(primeiro + " * " + segundo + " + " + terceiro +  " = " + resultado);
